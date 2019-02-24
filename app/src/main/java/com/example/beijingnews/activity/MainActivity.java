5a533e58fc1954f339eadcdfc645205448a765fa
rqsir <hbrq1314@gmail.com>
2019-02-24 11:34:46 +0800

    use a third-party module SlidingMenu to accomplish leftMenu

--- app/src/main/java/com/example/beijingnews/activity/MainActivity.java
+++ app/src/main/java/com/example/beijingnews/activity/MainActivity.java
@@ -4,12 +4,33 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.example.beijingnews.R;
+import com.example.beijingnews.utils.DensityUtil;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 1.configure main page
         setContentView(R.layout.activity_main);
+
+        // 2.configure leftMenu
+        setBehindContentView(R.layout.activity_leftmenu);
+
+        // 3.configure rightMenu
+        SlidingMenu slidingMenu = getSlidingMenu();
+        slidingMenu.setSecondaryMenu(R.layout.activity_rightmenu);
+
+        // 4.configure display mode: leftMenu + mainPage; leftMenu + mainPage + rightPage; rightMenu + mainPage
+        slidingMenu.setMode(SlidingMenu.LEFT);
+
+        // 5.configure sliding mode: slideMargin, slideFullScreen, noSlide
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+        // 6.configure the width of the mainPage while sliding
+        slidingMenu.setBehindOffset(DensityUtil.dip2px(MainActivity.this,200));
     }
 }
